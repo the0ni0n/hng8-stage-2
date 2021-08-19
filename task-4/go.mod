e65513594ee7756b4f5a9eafe572849f3197c7ea
the0ni0n <atikulate008@gmail.com>
2021-08-19 14:51:48 +0100

    task Done.

---
+++ task-4/go.mod
@@ -0,0 +1,3 @@
+module github.com/the0ni0n/qq/hng8-stage-2/task-4
+
+go 1.16
--- task-4/main.go
+++ task-4/main.go
@@ -1,1 +1,41 @@
 package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+var tpl *template.Template
+
+type ContactDetails struct {
+	Email   string
+	Subject string
+	Message string
+}
+
+func init() {
+	tpl = template.Must(template.ParseGlob("*.html"))
+	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets/"))))
+}
+
+func main() {
+	http.HandleFunc("/", index)
+	//http.HandleFunc("/contact", contact)
+	http.ListenAndServe(":8000", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != http.MethodPost {
+		tpl.ExecuteTemplate(w, "index.html", nil)
+		return
+	}
+
+	details := ContactDetails{
+		Email:   r.FormValue("email"),
+		Subject: r.FormValue("subject"),
+		Message: r.FormValue("message"),
+	}
+
+	tpl.ExecuteTemplate(w, "contact.html", details)
+}
